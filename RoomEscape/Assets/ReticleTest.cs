6f7fe9686f3339b5ba87f69eb11d8ceec0545dac
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-11-09 17:54:28 +0900

    RoomEscape

---
+++ RoomEscape/Assets/ReticleTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleTest : MonoBehaviour
+{
+    public void pointerEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void pointerExit()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
